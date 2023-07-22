fe23fe9b50580e72d69f5786ed3229381df90e62
Tạ Hải Lượng <10147245-luongth@users.noreply.gitlab.com>
2023-07-22 14:51:23 +0700

    js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+"use strict";
 // carousel
 // let navbar = document.querySelector('.navbar');
 
@@ -16,25 +17,44 @@ myApp.config(function ($routeProvider) {
   $routeProvider
 
     .when("/", {
-      templateUrl: "../views/home.html",
+      templateUrl: "views/home.html",
       controller: "homeCtrl",
     })
-    .when("/shop", {
-      templateUrl: "./views/shop.html",
-      controller: "shopCtrl",
+    .when("/home", {
+      templateUrl: "views/home.html",
+      controller: "homeCtrl",
+    })
+    .when("/shop/:nameProduct", {
+      templateUrl: "views/shop.html?" + Math.random(),
     })
     .otherwise({
       redirectTo: "/",
     })
     .when("/contact", {
-      templateUrl: "./views/contact.html",
+      templateUrl: "views/contact.html",
     })
     .when("/about", {
-      templateUrl: "./views/about.html",
+      templateUrl: "views/about.html",
+    })
+    .when("/blog", {
+      templateUrl: "views/blog.html",
+    })
+    .when("/cart", {
+      templateUrl: "views/cart.html",
+    })
+    .when("/detail", {
+      templateUrl: "views/detail.html",
+      controller: "detailCtrl",
+    })
+    .when("/login", {
+      templateUrl: "views/login.html",
+    })
+    .when("/user", {
+      templateUrl: "views/user.html",
+    })
+    .when("/blog-detail/:id", {
+      templateUrl: "views/blog-Detail.html",
     });
-  // .when("/detail", {
-  //   templateUrl: "./views/detail.html",
-  // });
 });
 myApp.run(function ($rootScope, $http) {
   $http.get("./data/data.json").then(function (response) {
@@ -46,6 +66,11 @@ myApp.run(function ($rootScope, $http) {
 myApp.factory("myService", function () {
   var savedData = {};
   var saveType = "";
+  var listCart = [];
+  var orderCart = [];
+  var user = {};
+  var orderList = [];
+
   // set data product
   function set(data) {
     savedData = data;
@@ -54,34 +79,96 @@ myApp.factory("myService", function () {
     return savedData;
   }
 
+  // set list cart
+  function setListCart(item) {
+    listCart.push(item);
+  }
+  // get list cart
+  function getListCart() {
+    return listCart;
+  }
   // save type product
   function setTypeProduct(type) {
     saveType = type;
   }
-  function getTypeproduct() {
+  function getTypeProduct() {
     return saveType;
+  }
+
+  //login
+  function loginUser(element) {
+    user = element;
+  }
+
+  function getUser() {
+    return user;
+  }
+
+  function logoutUser(data) {
+    const objWithIdIndex = user.findIndex((obj) => obj.id === data.id);
+    if (objWithIdIndex > -1) {
+      user.splice(objWithIdIndex, 1);
+    }
+    return user;
   }
 
   return {
     set: set,
     get: get,
+
     setTypeProduct: setTypeProduct,
-    getTypeproduct: getTypeproduct,
+    getTypeProduct: getTypeProduct,
+
+    //  myservice list cart
+    setListCart: setListCart,
+    getListCart: getListCart,
+
+    // login/logout user
+    loginUser: loginUser,
+    getUser: getUser,
+    logoutUser: logoutUser,
   };
 });
-// nav constroller
-myApp.controller("navCtrl", function ($scope) {
-  $scope.showNavbar = function () {
-    $(".nav").hide();
-    $(".nav-sidebar").show();
+myApp.filter("myFilter", function () {
+  return function (items, search) {
+    if (!search) {
+      return items;
+    }
+    var type = search;
+    if (!type || "shop" == type) {
+      return items;
+    }
+
+    if (search == "no" || search == "with") {
+      return items.filter(function (element, index) {
+        console.log(search);
+        return element.egg == search;
+      });
+    }
+
+    return items.filter(function (element, index) {
+      return element.type == search;
+    });
   };
-  $scope.hidenNavbar = function () {
-    $(".nav").show();
-    $(".nav-sidebar").hide();
+});
+// filte price
+myApp.filter("rangePrice", function () {
+  return function (items, rangePrice) {
+    var filteredValue = [];
+    var toPrice =
+      rangePrice.toPrice != undefined ? parseInt(rangePrice.toPrice) : 1;
+    var fromPrice =
+      rangePrice.fromPrice != undefined ? parseInt(rangePrice.fromPrice) : 100;
+    angular.forEach(items, function (item) {
+      if (item.price >= toPrice) {
+        filteredValue.push(item);
+      }
+    });
+    return filteredValue;
   };
 });
 // home controller
-myApp.controller("homeCtrl", function ($scope, myService) {
+myApp.controller("homeCtrl", function ($scope, myService, $http) {
   let fade = document.querySelectorAll(".fade");
 
   let index = 0;
@@ -102,43 +189,104 @@ myApp.controller("homeCtrl", function ($scope, myService) {
     myService.set(data);
   };
   // save type to myserevice
+  // function set list cart
+  $scope.setItemListCart = function (item) {
+    myService.setListCart(item);
+  };
+  $http.get("./data/blog.json").then(function (response) {
+    $scope.listBlogs = response.data.blog;
+  });
+});
+// nav constroller
+myApp.controller("navCtrl", function ($scope, myService) {
+  $scope.showNavbar = function () {
+    $(".nav").hide();
+    $(".nav-sidebar").show();
+  };
+  $scope.hidenNavbar = function () {
+    $(".nav").show();
+    $(".nav-sidebar").hide();
+  };
+  // save type to myService
   $scope.saveType = function (type) {
     myService.setTypeProduct(type);
   };
+  // get typeproduct
 });
+// app custommer filter
+
 // shop controller
-myApp.controller("shopCtrl", function ($scope, myService) {
-  // get type from myservice
-  $scope.typeProduct = myService.getTypeproduct();
-  console.log($scope.dataProduct);
-  // get data from filr json
+myApp.controller("shopCtrl", function ($scope, $routeParams, myService, $http) {
+  $scope.prop = $routeParams.nameProduct;
+  //pagination product
+  $scope.begin = 0;
+  $scope.pageSize = 12;
+  $scope.pageCount = Math.ceil($scope.dataProduct.length / $scope.pageSize);
 
-  // varible nagination page
-
-  ($scope.currentPage = 1), ($scope.pageSize = 12);
-
-  // get type from myservice
-
-  $scope.filter = $scope.typeProduct;
-  console.log($scope.filter);
-  $scope.getType = function (typeProduct) {
-    console.log(typeProduct);
-    $scope.type = typeProduct;
+  $scope.first = function () {
+    $scope.begin = 0;
   };
-  $scope.orderfilEgg = function (egg) {
-    $scope.egg = egg;
+  $scope.previous = function () {
+    if ($scope.begin > 0) {
+      $scope.begin -= $scope.pageSize;
+    }
+  };
+  $scope.next = function () {
+    if ($scope.begin < ($scope.pageSize - 1) * $scope.pageSize) {
+      $scope.begin += $scope.pageSize;
+    }
+  };
+  $scope.last = function () {
+    $scope.begin = ($scope.pageSize - 1) * $scope.pageSize;
+  };
+  $scope.sortBy = function (prop) {
+    $scope.prop = prop;
   };
 
-  //
+  // range input change background by price
+  $("#slider-range").slider({
+    range: true,
+    min: 0,
+    max: 200,
+    values: [1, 100],
+    slide: function (event, ui) {
+      $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+    },
+  });
+  $("#amount").val(
+    "$" +
+      $("#slider-range").slider("values", 0) +
+      " - $" +
+      $("#slider-range").slider("values", 1)
+  );
+
+  $("#slider-range-shop").slider({
+    range: true,
+    min: 0,
+    max: 200,
+    values: [1, 100],
+    slide: function (event, ui) {
+      $("#amount-shop").val("$" + ui.values[0] + " - $" + ui.values[1]);
+      $scope.to_price = ui.values[0];
+      $scope.from_price = ui.values[1];
+    },
+  });
+  $("#amount-shop").val(
+    "$" +
+      $("#slider-range-shop").slider("values", 0) +
+      " - $" +
+      $("#slider-range-shop").slider("values", 1)
+  );
 
   // save item data  myservice
   $scope.saveData = function (item) {
     myService.set(item);
   };
 
-  // range input change background by price
-
-  // filter product from data
+  // function set list cart
+  $scope.setItemListCart = function (item) {
+    myService.setListCart(item);
+  };
 });
 
 // contact controller
@@ -150,7 +298,7 @@ myApp.controller("detailCtrl", function ($scope, myService) {
 
   $scope.data.image == null
     ? ($scope.data.image =
-        "../images/Cute_girl_bakery_logo_homemade_bakery_shop_hand_drawn_cartoon_art_illustration.jpg")
+        "./images/Cute_girl_bakery_logo_homemade_bakery_shop_hand_drawn_cartoon_art_illustration.jpg")
     : null;
 
   // get by class tag
@@ -177,23 +325,119 @@ myApp.controller("detailCtrl", function ($scope, myService) {
   };
   // zoom img product
 
-  // let magnifying_area = document.getElementById("magnifying_area");
-  // let magnifying_img = document.getElementById("magnifying_img");
-  // magnifying_area.addEventListener("mousemove", function (event) {
-  //   let clientX = event.clientX - magnifying_area.offsetLeft;
-  //   let clinetY = event.clientY - magnifying_area.offsetTop;
+  let magnifying_area = document.getElementById("magnifying_area");
+  let magnifying_img = document.getElementById("magnifying_img");
+  magnifying_area.addEventListener("mousemove", function (event) {
+    let clientX = event.clientX - magnifying_area.offsetLeft;
+    let clinetY = event.clientY - magnifying_area.offsetTop;
 
-  //   mWidth = magnifying_area.offsetWidth;
-  //   mHeight = magnifying_area.offsetHeight;
+    let mWidth = magnifying_area.offsetWidth;
+    let mHeight = magnifying_area.offsetHeight;
 
-  //   clientX = (clientX / mWidth) * 100;
-  //   clinetY = (clinetY / mWidth) * 100;
+    clientX = (clientX / mWidth) * 100;
+    clinetY = (clinetY / mWidth) * 100;
 
-  //   magnifying_img.style.transform =
-  //     "translate(-" + clientX + "%,-" + clinetY + "%) scale(2.4)";
-  // });
-  // magnifying_area.addEventListener("mouseleave", function () {
-  //   magnifying_img.style.transform = "translate(-50%,-50%) scale(1)";
-  // });
+    magnifying_img.style.transform =
+      "translate(-" + clientX + "%,-" + clinetY + "%) scale(2.4)";
+  });
+  magnifying_area.addEventListener("mouseleave", function () {
+    magnifying_img.style.transform = "translate(-50%,-50%) scale(1)";
+  });
 });
 myApp.controller("aboutCtrl", function ($scope) {});
+
+// blog controller
+myApp.controller("blogCtrl", function ($scope, $http) {
+  $http.get("./data/blog.json").then(function (response) {
+    $scope.listBlog = response.data.blog;
+  });
+});
+
+myApp.controller("cartCtrl", function ($scope, myService) {
+  // get list cart
+  let list = [];
+  list = myService.getListCart();
+
+  let myArrayWithNoDuplicates = list.reduce(function (accumulator, element) {
+    if (accumulator.indexOf(element) === -1) {
+      accumulator.push(element);
+    }
+    return accumulator;
+  }, []);
+
+  // total order
+  $scope.totalOrder = function () {
+    var total = 0;
+    myArrayWithNoDuplicates.forEach((element) => {
+      total += element.price * element.amount;
+    });
+    return total;
+  };
+  // remover item cart
+  $scope.removeItem = function (item) {
+    myArrayWithNoDuplicates.forEach((element, index) => {
+      if (element.id == item.id) {
+        myArrayWithNoDuplicates.splice(index, 1);
+      }
+    });
+  };
+  $scope.listCart = myArrayWithNoDuplicates;
+});
+
+/**
+ * LOGIN CONTROLLER
+ * **/
+myApp.controller("loginCtrl", function ($scope, myService, $http) {
+  var listUser = [];
+  var user = {
+    email: "",
+    password: false,
+  };
+  $http.get("./data/user.json").then(function (response) {
+    listUser = response.data.details;
+  });
+
+  // login
+  console.log(listUser);
+  //validateEmail
+  function validateEmail(email) {
+    let res = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return res.test(email);
+  }
+
+  $scope.login = function () {
+    user.email = $scope.email;
+    user.password = $scope.password;
+
+    let item = listUser.filter((el) => el.password == user.password);
+    if (item.length == 1) {
+      myService.loginUser(item);
+
+      $("#to-user").attr("href", "#!/user");
+    } else {
+      $scope.check = true;
+    }
+  };
+
+  //logout
+  $scope.logout = function (data) {
+    myService.logoutUser(data);
+  };
+});
+// controoler userCrtl
+
+myApp.controller("userCtrl", function ($scope, myService) {
+  // get my service  user
+  $scope.items = myService.getUser();
+});
+// detail blog
+myApp.controller("blogDetailCtrl", function ($scope, $routeParams, $http) {
+  var listDetailBlog = [];
+  let idBlog = $routeParams.id;
+
+  $http.get("./data/detail-blog.json").then(function (response) {
+    listDetailBlog = response.data.blogDetail;
+    $scope.itemBlog = listDetailBlog.filter((item) => item.id == idBlog);
+    console.log($scope.itemBlog);
+  });
+});
